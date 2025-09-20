9cbdf185d9bfead12cc096afbfb22d9f1fb938d8
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-09-20 23:59:02 +0530

    Update GovernanceToken.sol

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";    
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";      
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
