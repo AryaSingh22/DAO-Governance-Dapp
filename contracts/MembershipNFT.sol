6da72935f4ad62c587272c127ed346e652dbcb36
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-09-27 00:32:03 +0530

    Update MembershipNFT.sol

--- contracts/MembershipNFT.sol
+++ contracts/MembershipNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";   
-   
+    
 /**
  * DAO Membership NFT - represents membership in the DAO
  * Can be minted by governance proposals or by meeting certain criteria
