dbdad39aeddff98c836b07efe129333a3ba1b6bb
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-12-03 23:53:40 +0530

    Update VotingEngine.sol

--- contracts/governance/VotingEngine.sol
+++ contracts/governance/VotingEngine.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.24; 
 
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import {Governor} from "@openzeppelin/contracts/governance/Governor.sol"; 
@@ -75,4 +75,5 @@ contract VotingEngine {
     }
 
 }
+
 
