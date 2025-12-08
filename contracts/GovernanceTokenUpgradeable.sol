9546489e5eac87fd212f9ee595eac61bdcafdd14
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-12-08 23:36:45 +0530

    Update GovernanceTokenUpgradeable.sol

--- contracts/GovernanceTokenUpgradeable.sol
+++ contracts/GovernanceTokenUpgradeable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24; 
- 
+  
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
