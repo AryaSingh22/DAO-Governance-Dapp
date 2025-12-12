a2fa2b7fb3bd76953989aec64459ccc34fe34318
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-12-12 23:36:24 +0530

    Update ReputationManager.sol

--- contracts/ReputationManager.sol
+++ contracts/ReputationManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24; 
+pragma solidity ^0.8.24;
  
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol"; 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -255,6 +255,7 @@ contract ReputationManager is ERC721, ERC721URIStorage, Ownable {
     }
 
 }
+
 
 
 
