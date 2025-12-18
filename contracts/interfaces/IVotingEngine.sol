1401108f5a518d0bf009551c039bfd1ee8d02207
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-12-18 23:51:42 +0530

    Update IVotingEngine.sol

--- contracts/interfaces/IVotingEngine.sol
+++ contracts/interfaces/IVotingEngine.sol
@@ -5,13 +5,14 @@ interface IVotingEngine {
     // Events
     event QuadraticVoteCast(address indexed voter, uint256 proposalId, uint256 votes, uint256 sqrtVotes);
     event DelegationHierarchyCreated(address indexed delegator, address indexed delegate);
-    
+     
     // Functions
     function castQuadraticVote(uint256 proposalId, uint256 votes) external returns (uint256 sqrtVotes);
     function createDelegation(address delegate) external;
     function getDelegationChain(address delegator) external view returns (address[] memory);
 
 }
+
 
 
 
