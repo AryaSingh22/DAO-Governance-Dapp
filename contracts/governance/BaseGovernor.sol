7411db48d8cf16d4f644b428e501a301b15e82b0
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2026-01-20 23:53:28 +0530

    Update BaseGovernor.sol

--- contracts/governance/BaseGovernor.sol
+++ contracts/governance/BaseGovernor.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {Governor} from "@openzeppelin/contracts/governance/Governor.sol"; 
 import {GovernorSettings} from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
-import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol"; 
 import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import {GovernorTimelockControl} from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
@@ -139,6 +139,7 @@ contract BaseGovernor is
     }
 
 }
+
 
 
 
